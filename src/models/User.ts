5cf6de0f958868f0c53583d7b64b4acfdeb58d1b
Hala <halakhellow1996@gmail.com>
2021-07-25 23:16:11 +0300

    add save method to User class in User.ts file to save information tied to a user

--- src/models/User.ts
+++ src/models/User.ts
@@ -41,4 +41,14 @@ export class User {
         this.set(response.data);
       });
   }
+
+  save(): void {
+    let id = this.get("id");
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post("http://localhost:3000/users", this.data);
+    }
+  }
 }
